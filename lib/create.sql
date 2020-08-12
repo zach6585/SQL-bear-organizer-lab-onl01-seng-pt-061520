c3801090d08aecea93d950e728ae4a52a0b1fdab
zachary mines <zmines@wesleyan.edu>
2020-08-12 22:37:29 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age TEXT,
+gender TEXT,
+color TEXT,
+temperament TEXT,
+alive INTEGER
+);
---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Mr. Chocolate", 23, "male","blue","angry","1");
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 12, "male","green","angry","1");
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Tabitha", 2, "female","blue","calm","1");
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Sergeant Brown", 27, "non-binary","yellow","sleepy","1");
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Melissa", 21, "female","orange","dopey","1");
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Grinch", 17, "male","brown","angry","1");
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Wendy", 13, "female","black","hungry","1");
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (NULL, 233, "male","white","peeved","1");
